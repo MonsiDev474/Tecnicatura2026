d569247ef7d7216b1074abc2487145d27cc4811f
manuel_monsalve <mmonsalve1357@gmail.com>
2026-04-15 21:46:59 -0300

    add java and python directories add class practice

---
+++ java/EjemploHolaMundo/src/HolaMundo.java
@@ -0,0 +1,10 @@
+// this is a comment
+/*
+   this is a 
+   longer comment
+*/
+public class HolaMundo {
+    public static void main(String args[]) {
+        System.out.println("Hello World JAVA");
+    }
+}
---
+++ java/leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,15 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde java!");
+        // tipo int
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        // tipo string
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+}
