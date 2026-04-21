de30a2526e3dfb077e525264eeb20bf395e82d8f
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-04-20 22:22:33 -0300

    add javascript practice clase 2

---
+++ javaScript/leccion01/01-01-holamundo.js
@@ -0,0 +1,15 @@
+
+var name = "Manu";
+name = "Alejandro Manuel";
+var surname = "Monsalve";
+
+// Mostrando por pantalla individualmente
+// console.log(name);
+// console.log(surname);
+
+// Mostrando por pantalla concatenado
+// console.log(name + " " +surname);
+
+// Mostrando con saltos de linea
+name = "Alejandro\nManuel\n";
+console.log(name + surname);
---
+++ javaScript/leccion01/HolaMundo.js
@@ -0,0 +1,3 @@
+var name = "Manu";
+name = "Hola mundo desde javascript";
+console.log(name);
