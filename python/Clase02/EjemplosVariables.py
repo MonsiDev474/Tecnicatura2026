75f2bf82a058bb8c91bee4bd847828d23d4c7e68
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-04-22 22:00:31 -0300

    fix folder structure for javascript and python

---
+++ python/Clase02/EjemplosVariables.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    # ------------------------------------------------------------------------------------
+    miVariable = 3
+    print(miVariable)
+    miVariable = "Hola mundo"
+    print(miVariable)
+    miVariable = 3.5
+    print(miVariable)
+    x = 10
+    y = 2
+    z = x + y
+    print(z)
+    print(id(x)) # las literales se escriben "x984" (últimos tres caracteres, dirección de memoria normalmente son hexadecimal) <- cambia al volver a ejecutar
+    print(id(y))
+    print(id(z))
