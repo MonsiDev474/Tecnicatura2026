0c4b2ff7409766b104b907ac0caf4c671428c171
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-05-11 21:01:10 -0300

    add simplified if-else structure example (class 06 python)

---
+++ python/Clase06/Ejemplo_Opera_Ternario.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    condicion = True
+    # ejemplo operador ternario (if-else en una sola línea, pero NO elif)
+    print("Condicion verdadera") if condicion else print("Condicion falsa")
