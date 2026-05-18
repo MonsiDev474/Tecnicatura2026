bf190dd8155fbf46b91aa096ebdcd0bf689a55c0
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-05-18 20:18:15 -0300

    add Python class 7 - First exercise

---
+++ python/Clase07/Ejercicio3_estacion_del_anio.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    mes_del_anio = None
+    while mes_del_anio is None:
+        mes_del_anio = int(input("Ingrese un mes del año (1-12): "))
+        if not 1 <= mes_del_anio <= 12:
+            print("Mes no valido, ingrese de nuevo un numero del 1-12")
+            mes_del_anio = None
+    if 1 <= mes_del_anio <= 3:
+        print("Usted está en Verano")
+    elif 4 <= mes_del_anio <= 6:
+        print("Usted está en Otoño")
+    elif 7 <= mes_del_anio <= 9:
+        print("Usted está en Invierno")
+    elif 10 <= mes_del_anio <= 12:
+        print("Usted está en Primavera")
