255c6cbf749c402a6d569516d2209ff0963d5bfb
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-05-18 21:27:05 -0300

    modify Python class7 - exercises 3 and 4

--- python/Clase07/Ejercicio3_estacion_del_anio.py
+++ python/Clase07/Ejercicio3_estacion_del_anio.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
         if not 1 <= mes_del_anio <= 12:
             print("Mes no valido, ingrese de nuevo un numero del 1-12")
             mes_del_anio = None
-    if 1 <= mes_del_anio <= 3:
-        print("Usted está en Verano")
-    elif 4 <= mes_del_anio <= 6:
-        print("Usted está en Otoño")
-    elif 7 <= mes_del_anio <= 9:
-        print("Usted está en Invierno")
-    elif 10 <= mes_del_anio <= 12:
-        print("Usted está en Primavera")
+    else:
+        if 1 <= mes_del_anio <= 3:
+            print("Usted está en Verano")
+        elif 4 <= mes_del_anio <= 6:
+            print("Usted está en Otoño")
+        elif 7 <= mes_del_anio <= 9:
+            print("Usted está en Invierno")
+        elif 10 <= mes_del_anio <= 12:
+            print("Usted está en Primavera")
--- python/Clase07/Ejercicio4_etapas_de_vida.py
+++ python/Clase07/Ejercicio4_etapas_de_vida.py
@@ -5,22 +5,22 @@ if __name__ == "__main__":
         if edad < 0:
             print("Por favor ingrese una edad valida (>0)")
             edad = None
-
-    if 0 <= edad <= 10:
-        print("La infancia es increíble y bella")
-    elif 11 <= edad <= 19:
-        print("Tienes muchos cambios, mucho que estudiar")
-    elif 20 <= edad <= 29:
-        print("Amor y comienza el trabajo")
-    elif 30 <= edad <= 49:
-        print("Maduración terminada, quizás encuentras tu lugar en el mundo")
-    elif 50 <= edad <= 60:
-        print("No ves la hora de jubilarte")
-    elif 61 <= edad <= 79:
-        print("Tiempo para hacer lo que nunca pudiste")
-    elif 80 <= edad <= 89:
-        print("Comienzan las limitaciones físicas")
-    elif 90 <= edad <= 110:
-        print("Los últimos momentos")
     else:
-        print("Sos inmortal o qué!?")
+        if 0 <= edad <= 10:
+            print("La infancia es increíble y bella")
+        elif 11 <= edad <= 19:
+            print("Tienes muchos cambios, mucho que estudiar")
+        elif 20 <= edad <= 29:
+            print("Amor y comienza el trabajo")
+        elif 30 <= edad <= 49:
+            print("Maduración terminada, quizás encuentras tu lugar en el mundo")
+        elif 50 <= edad <= 60:
+            print("No ves la hora de jubilarte")
+        elif 61 <= edad <= 79:
+            print("Tiempo para hacer lo que nunca pudiste")
+        elif 80 <= edad <= 89:
+            print("Comienzan las limitaciones físicas")
+        elif 90 <= edad <= 110:
+            print("Los últimos momentos")
+        else:
+            print("Sos inmortal o qué!?")
